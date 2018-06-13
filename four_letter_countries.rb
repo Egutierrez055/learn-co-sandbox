b498b756c17300843e19ea9718140419520f401b
Egutierrez055 <egutierrez055@gmail.com>
2018-06-13 17:16:09 +0000

    automatically backed up by learn

---
+++ four_letter_countries.rb
@@ -0,0 +1,8 @@
+four_letter_countries = [ "Chad", "Cuba", "Greenland", "Iraq", "Mali", "Oman", "India"]
+four_letter_countries.push ("Fiji")
+
+four_letter_countries= []
+four_letter_countries.push "Iran"
+four_letter_countries.push([4])
+
+puts four_letter_countries
